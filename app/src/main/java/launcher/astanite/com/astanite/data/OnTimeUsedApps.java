1942cb3afa21b5b1520f528225bf1e28324a5018
shivamkumard107 <shivam.kumard107@gmail.com>
2018-12-14 13:21:43 +0530

    user's app data retrieved

--- app/src/main/java/launcher/astanite/com/astanite/data/OnTimeUsedApps.java
+++ app/src/main/java/launcher/astanite/com/astanite/data/OnTimeUsedApps.java
@@ -1,19 +1,31 @@
 package launcher.astanite.com.astanite.data;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class OnTimeUsedApps {
     private String packageName ;
-    private long CurrentTimeMillis ;
+    private String currentTime;
 
     public OnTimeUsedApps(String packageName, long currentTimeMillis) {
         this.packageName = packageName;
-        CurrentTimeMillis = currentTimeMillis;
+        convertMilliseconds(currentTimeMillis);
     }
 
+    private void convertMilliseconds(long currentTimeMillis) {
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Kolkata"));
+        currentTime = sdf.format(new Date(currentTimeMillis));
+    }
+
+    public String getAppOpenedTime() {
+        return currentTime;
+    }
     public String getPackageName() {
-        return packageName;
+        return this.packageName;
     }
 
-    public void setPackageName(String packageName) {
-        this.packageName = packageName;
-    }
+
 }
