60e618d71d0bca4582a62590bd2255ab121dbdb3
Utsav <utsav24chawla@gmail.com>
2019-01-08 15:43:32 +0530

    Animations edited| app drawer button edited

--- app/src/main/java/launcher/astanite/com/astanite/ui/DataAnalysisFragment.java
+++ app/src/main/java/launcher/astanite/com/astanite/ui/DataAnalysisFragment.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.SharedElementCallback;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
