bf19775463c5736bca93c6e5c2c46a30422813b9
Utsav <utsav24chawla@gmail.com>
2019-01-11 14:46:19 +0530

    Mode and Distractive app list update with uninstall

--- app/src/main/java/launcher/astanite/com/astanite/utils/BroadCastReceiver.java
+++ app/src/main/java/launcher/astanite/com/astanite/utils/BroadCastReceiver.java
@@ -3,10 +3,24 @@ package launcher.astanite.com.astanite.utils;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ResolveInfo;
+import android.util.Log;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import javax.security.auth.login.LoginException;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class BroadCastReceiver extends BroadcastReceiver
 {
     SendToHomeActivity mListener;
+    ArrayList<String> dapp;
+    HashSet<String> dnewapp;
 
     @SuppressWarnings("unused")
     public BroadCastReceiver()
@@ -23,10 +37,147 @@ public class BroadCastReceiver extends BroadcastReceiver
     {
         if(mListener!=null)
             mListener.sendToHomeActivity();
-    }
 
+        List<ResolveInfo> apps = getapps(context);
+
+        updatedistractive(context, apps);
+        updatefocus(context, apps);
+        updateleisure(context, apps);
+        updatesleep(context, apps);
+
+    }
     public interface SendToHomeActivity
     {
         void sendToHomeActivity();
+    }
+
+    private List<ResolveInfo> getapps(Context context) {
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> pkgAppsList = context.getPackageManager().queryIntentActivities(mainIntent, 0);
+        return pkgAppsList;
+    }
+
+    private void updatedistractive(Context context, List<ResolveInfo> apps)
+    {
+
+
+        dapp = new ArrayList<>();
+        dnewapp = new HashSet<>();
+        dapp.addAll(context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, MODE_PRIVATE).getStringSet(Constants.KEY_DISTRACTIVE_APPS, new HashSet<>()));
+
+        for(int i =0; i<dapp.size(); i++)
+        {
+            boolean flag = false;
+            for(int j =0; j<apps.size(); j++)
+            {
+                if(dapp.get(i).equals(apps.get(j).activityInfo.packageName))
+                {
+                    flag = true;
+                }
+            }
+
+            if(flag)
+            {
+                dnewapp.add(dapp.get(i));
+            }
+        }
+
+        Log.e("Old",Integer.toString(dapp.size()) );
+        Log.e("New", Integer.toString(dnewapp.size()));
+        context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, MODE_PRIVATE).edit().putStringSet(Constants.KEY_DISTRACTIVE_APPS, dnewapp).apply();
+    }
+
+    private void updatefocus(Context context, List<ResolveInfo> apps)
+    {
+
+
+        ArrayList <String> dapp = new ArrayList<>();
+        HashSet <String> dnewapp = new HashSet<>();
+        dapp.addAll(context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, MODE_PRIVATE).getStringSet(Constants.KEY_FOCUS_APPS, new HashSet<>()));
+
+        for(int i =0; i<dapp.size(); i++)
+        {
+            boolean flag = false;
+            for(int j =0; j<apps.size(); j++)
+            {
+                if(dapp.get(i).equals(apps.get(j).activityInfo.packageName))
+                {
+                    flag = true;
+                }
+            }
+
+            if(flag)
+            {
+                dnewapp.add(dapp.get(i));
+            }
+        }
+
+        Log.e("Old",Integer.toString(dapp.size()) );
+        Log.e("Apparentnew", Integer.toString(dnewapp.size()));
+        context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, MODE_PRIVATE).edit().putStringSet(Constants.KEY_FOCUS_APPS, dnewapp).apply();
+        Log.e("New", Integer.toString(context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, MODE_PRIVATE).getStringSet(Constants.KEY_FOCUS_APPS, new HashSet<>()).size()));
+    }
+
+    private void updateleisure(Context context, List<ResolveInfo> apps)
+    {
+
+
+        ArrayList <String> dapp = new ArrayList<>();
+        HashSet <String> dnewapp = new HashSet<>();
+        dapp.addAll(context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, MODE_PRIVATE).getStringSet(Constants.KEY_MY_MODE_APPS, new HashSet<>()));
+
+        for(int i =0; i<dapp.size(); i++)
+        {
+            boolean flag = false;
+            for(int j =0; j<apps.size(); j++)
+            {
+                if(dapp.get(i).equals(apps.get(j).activityInfo.packageName))
+                {
+                    flag = true;
+                }
+            }
+
+            if(flag)
+            {
+                dnewapp.add(dapp.get(i));
+            }
+        }
+
+        Log.e("Old",Integer.toString(dapp.size()) );
+        Log.e("Apparentnew", Integer.toString(dnewapp.size()));
+        context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, MODE_PRIVATE).edit().putStringSet(Constants.KEY_MY_MODE_APPS, dnewapp).apply();
+        Log.e("New", Integer.toString(context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, MODE_PRIVATE).getStringSet(Constants.KEY_MY_MODE_APPS, new HashSet<>()).size()));
+    }
+
+    private void updatesleep(Context context, List<ResolveInfo> apps)
+    {
+
+
+        ArrayList <String> dapp = new ArrayList<>();
+        HashSet <String> dnewapp = new HashSet<>();
+        dapp.addAll(context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, MODE_PRIVATE).getStringSet(Constants.KEY_SLEEP_APPS, new HashSet<>()));
+
+        for(int i =0; i<dapp.size(); i++)
+        {
+            boolean flag = false;
+            for(int j =0; j<apps.size(); j++)
+            {
+                if(dapp.get(i).equals(apps.get(j).activityInfo.packageName))
+                {
+                    flag = true;
+                }
+            }
+
+            if(flag)
+            {
+                dnewapp.add(dapp.get(i));
+            }
+        }
+
+        Log.e("Old",Integer.toString(dapp.size()) );
+        Log.e("Apparentnew", Integer.toString(dnewapp.size()));
+        context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, MODE_PRIVATE).edit().putStringSet(Constants.KEY_SLEEP_APPS, dnewapp).apply();
+        Log.e("New", Integer.toString(context.getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, MODE_PRIVATE).getStringSet(Constants.KEY_SLEEP_APPS, new HashSet<>()).size()));
     }
 }
